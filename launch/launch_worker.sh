7655f8f3db4c38ab7549c964ef074d410da01036
IAMXII <1923354735@qq.com>
2025-07-09 20:20:00 +0800

    second commit

--- launch/launch_worker.sh
+++ launch/launch_worker.sh
@@ -13,5 +13,5 @@ export RANK=$((RANK_ID * GPUS_PER_NODE))
 export WORLD_SIZE=$((NUM_NODES * GPUS_PER_NODE))
 #export CUDA_LAUNCH_BLOCKING=1
 
-#deepspeed --num_gpus=${GPUS_PER_NODE} ${TRAIN_FILE} ${RUN_ARGS}
-deepspeed --hostfile hostfile.txt --num_gpus=${GPUS_PER_NODE} --num_nodes=${NUM_NODES} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT}  ${TRAIN_FILE} ${RUN_ARGS}
+deepspeed --num_gpus=${GPUS_PER_NODE} ${TRAIN_FILE} ${RUN_ARGS}
+#deepspeed --hostfile hostfile.txt --num_gpus=${GPUS_PER_NODE} --num_nodes=${NUM_NODES} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT}  ${TRAIN_FILE} ${RUN_ARGS}
