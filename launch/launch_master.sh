a292c3bf93b9efdc6bfe5306171aaa74500145fc
IAMXII <1923354735@qq.com>
2025-07-10 23:13:42 +0800

    second commit

--- launch/launch_master.sh
+++ launch/launch_master.sh
@@ -6,7 +6,4 @@ export WORLD_SIZE=$((NUM_NODES * GPUS_PER_NODE))
 
 echo "准备启动 DeepSpeed 多机训练..."
 deepspeed   --num_gpus=${GPUS_PER_NODE} \
-  --num_nodes=${NUM_NODES} \
-  --master_addr=${MASTER_ADDR} \
-  --master_port=${MASTER_PORT} \
   ${TRAIN_FILE} ${RUN_ARGS}
