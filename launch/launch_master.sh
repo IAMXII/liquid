c7ce758dcfb9d65f53e6b22334bbf6284572bc1a
IAMXII <1923354735@qq.com>
2025-07-10 16:04:37 +0800

    second commit

--- launch/launch_master.sh
+++ launch/launch_master.sh
@@ -34,5 +34,5 @@ export MASTER_PORT=${MASTER_PORT}
 export RANK=0
 export WORLD_SIZE=$((NUM_NODES * GPUS_PER_NODE))
 #export CUDA_LAUNCH_BLOCKING=1
-
+echo ${GPUS_PER_NODE}
 deepspeed --hostfile hostfile.txt --num_gpus=${GPUS_PER_NODE} --num_nodes=${NUM_NODES} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT}  ${TRAIN_FILE} ${RUN_ARGS}
