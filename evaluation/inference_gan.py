47a61d67ea7c88c412a1c6c68220769be66759da
IAMXII <1923354735@qq.com>
2025-07-21 19:13:03 +0800

    second commit

--- evaluation/inference_gan.py
+++ evaluation/inference_gan.py
@@ -481,7 +481,7 @@ if __name__ == '__main__':
     parser.add_argument('--cfg', type=float, default=7.0, help='Classifier-Free Guidance scale')
     parser.add_argument('--TopP', type=float, default=0.96, help='Top P, max=1.0')
     parser.add_argument('--TopK', type=int, default=4096, help='Top K, max=264192')
-    parser.add_argument('--temperature', type=float, default=0.99, help='sampling temperature, max=1.0')
+    parser.add_argument('--temperature', type=float, default=0.1, help='sampling temperature, max=1.0')
 
     args = parser.parse_args()
     main(args)
