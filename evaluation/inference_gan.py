bcf39a94172a15f65b9af16ba54e1dca6ea9063c
IAMXII <1923354735@qq.com>
2025-07-22 16:55:29 +0800

    second commit

--- evaluation/inference_gan.py
+++ evaluation/inference_gan.py
@@ -98,9 +98,9 @@ def build_vqa_inference_input(tokenizer, sources):
     from liquid import conversation as conversation_lib
     conv = conversation_lib.default_conversation.copy()
     conv.append_message(conv.roles[0], human_text)
-    conv.append_message(conv.roles[1], "")  # 模型生成
+    conv.append_message(conv.roles[1], "<boi>")  # 模型生成
     prompt = conv.get_prompt()
-    prompt += " "
+    # prompt += " "
     # 原始 input_ids 和 attention_mask（还不含图像 token）
     encoded = tokenizer(prompt, return_tensors="pt", padding=True)
     input_ids = encoded["input_ids"][0].tolist()  # 转为 list 方便插入
@@ -112,7 +112,7 @@ def build_vqa_inference_input(tokenizer, sources):
         output_mask = []
         i = 0
         vq_iter = iter(vqcode_list)
-        while i < len(input_ids):
+        while i < len(input_ids)-1:
             token = input_ids[i]
             token_str = tokenizer.convert_ids_to_tokens(token)
             if token_str == "<boi>":
@@ -415,8 +415,8 @@ def main(args):
         image_insert_pos = []
         boi_token_id = tokenizer.convert_tokens_to_ids("<boi>")
         num_img_tokens = 256
-        generating_image_tokens = False
-        image_tokens_remaining = 0
+        generating_image_tokens = True
+        image_tokens_remaining = 256
         for i in tqdm(range(1626)):
             model_inputs = vqllm.prepare_inputs_for_generation(input_ids, **model_kwargs)
             outputs = vqllm(**model_inputs, return_dict=True)
