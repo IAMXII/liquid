3f7ff1a6931da17ccf9cca3e6c6ec82eace6876e
IAMXII <1923354735@qq.com>
2025-07-22 16:00:30 +0800

    second commit

--- evaluation/inference_gan.py
+++ evaluation/inference_gan.py
@@ -38,12 +38,6 @@ def sample_lw(logits, temperature: float = 1.0, top_k: int = 0, top_p: float = 1
     if temperature != 1.0:
         logits = logits / temperature
 
-    # Top-k 筛选
-    if top_k > 0:
-        values, _ = torch.topk(logits, top_k)
-        min_values = values[:, -1].unsqueeze(-1)
-        logits = torch.where(logits < min_values, torch.full_like(logits, float('-inf')), logits)
-
     # Top-p 筛选（Nucleus Sampling）
     if top_p < 1.0:
         sorted_logits, sorted_indices = torch.sort(logits, descending=True)
