c6962c45e59dfb04e0e911773929e3401558fd77
IAMXII <1923354735@qq.com>
2025-07-22 17:50:19 +0800

    second commit

--- evaluation/inference_gan.py
+++ evaluation/inference_gan.py
@@ -449,19 +449,19 @@ def main(args):
             if generating_image_tokens:
                 next_token, _ = sample(next_token_logits, **sampling_kwargs)
                 image_tokens_remaining -= 1
-
+                print("nextToken1:", next_token)
                 if image_tokens_remaining == 0:
                     generating_image_tokens = False
-                    print("nextToken1:", next_token)
 
-                else:
-                    # 普通文本逻辑，只保留前256000个token
-                    logits = next_token_logits[:, :, :256000]
-                    # probs = F.softmax(logits, dim=-1)
-                    # max_prob, max_idx = torch.max(probs, dim=-1)
-                    # next_token = max_idx
-                    next_token, _ = sample_lw(logits, **sampling_kwargs)
-                    print("nextToken2:", next_token)
+
+            else:
+                # 普通文本逻辑，只保留前256000个token
+                logits = next_token_logits[:, :, :256000]
+                # probs = F.softmax(logits, dim=-1)
+                # max_prob, max_idx = torch.max(probs, dim=-1)
+                # next_token = max_idx
+                next_token, _ = sample_lw(logits, **sampling_kwargs)
+                print("nextToken2:", next_token)
 
                 # 如果输出了 <boi>，进入图像生成状态
                 if next_token.item() == boi_token_id:
